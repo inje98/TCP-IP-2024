8dc35d5ce2148367d94c534843f0b80694e7d9f4
inje98 <kij980723@gmail.com>
2024-06-19 11:14:11 +0900

    ..

---
+++ Test/TestServer2.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 10001
+
+void error_handling(char *message);
+
+
+char webpage[] =
+    "HTTP/1.1 200 OK\r\n"
+    "Server: Linux Web Server\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+    "<!DOCTYPE html>\r\n"
+    "<html><head><title>My Web Page</title>\r\n"
+    "<style>body {background-color: #FFFF00; }</style></head>\r\n"
+    "<body><center><h1>Hello world!!</h1><br>\r\n"
+    "<img src=\"game.jpg\"></center></body></html>\r\n";
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[30000] = {0};
+
+    // 소켓 파일 디스크립터 생성
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 소켓 옵션 설정
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // 주소 설정
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(PORT);
+
+    // 소켓에 주소 바인딩
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // 요청 대기
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        printf("Waiting for connections ...\n");
+
+        // 클라이언트 연결 수락
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // 클라이언트 요청 읽기
+        read(new_socket, buffer, sizeof(buffer));
+        printf("Received request:\n%s\n", buffer);
+
+        // HTTP GET 요청에 대해 응답 전송
+        if (strncmp(buffer, "GET", 3) == 0) {
+            write(new_socket, webpage, sizeof(webpage) - 1);
+        }
+
+        close(new_socket);
+    }
+
+    close(server_fd);
+    return 0;
+}
+
+void error_handling(char* message)
+ {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+ }
